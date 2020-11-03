ProjectStreak-Devices/device_xiaomi_raphael

raphael: BoardCOnfig: Adapt and change QCOM's sepolicy makefile path
Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

BoardConfig.mk
@@ -149,12 +149,7 @@ DISABLE_RILD_OEM_HOOK := true
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     $(DEVICE_PATH)/sepolicy/private \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true

Raaj52/device_tree_raphael

BoardConfig.mk
@@ -147,14 +147,8 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 DISABLE_RILD_OEM_HOOK := true
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(DEVICE_PATH)/sepolicy/private \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+include device/qcom/sepolicy/SEPolicy.mk
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true